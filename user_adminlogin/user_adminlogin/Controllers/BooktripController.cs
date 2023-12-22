3db15198097dc1d547af69e7ecf2f7dbfc33e20d
mhmdmahdi <mhmdmahdi76931@gmail.com>
2023-12-22 13:58:52 +0200

    mhmd Commit

--- user_adminlogin/user_adminlogin/Controllers/BooktripController.cs
+++ user_adminlogin/user_adminlogin/Controllers/BooktripController.cs
@@ -1,30 +1,60 @@
 ﻿
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Security.Claims;
+using user_adminlogin.Data;
+using user_adminlogin.Models;
 
 namespace Flight_dbproject.Controllers
 {
     [Authorize]
-
-
     public class BooktripController : Controller
     {
-        public BooktripController()
-        {
-          
-        }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _db;
 
+        public BooktripController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
         public IActionResult Index()
         {
-            //// Retrieve unique destinations from the database
-            //var destinations = _dbContext.Flights.Distinct().ToList();
-
-            //// Pass the destinations to the view
-            //ViewBag.Destinations = destinations;
 
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Index(Flight flight)
+        {
+            var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            /* ApplicationUser user = from _userManager
+                         where Id = id
+                         select * ;*/
+            var booking = new Booking
+            {
+                UserId = id,
+                User = user,
+                FlightId = flight.Id,
+                Flight = flight,
+            };
+            _db.Bookings.Add(booking);
+            _db.Flights.Add(flight);
+            _db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult bookingList()
+        {
+            var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var bookings = from row in _db.Bookings
+                           where row.UserId == id
+                           select row;
+            List<Booking> bookingList = bookings.ToList();
+            return View(bookingList);
+        }
     }
 }
